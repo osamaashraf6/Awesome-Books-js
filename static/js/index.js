eb791d55ecfe1312c63d38eb92e2dcf1b494a74b
osamaashraf6 <osamaashraf668@gmail.com>
2022-10-25 21:55:10 +0200

    Add the remove method in the class

--- static/js/index.js
+++ static/js/index.js
@@ -1,66 +1,4 @@
-// let books = [];
-// // ==================================================================
-// const form = document.querySelector('form');
-// const booksContainer = document.querySelector('.books-container');
-// // ==================================================================
-// const showBook = (item) => {
-//   if (booksContainer.innerHTML === 'No thing books here') {
-//     booksContainer.innerHTML = '';
-//   }
-//   const bookContent = document.createElement('div');
-//   const text = `
-//     <p class="book-title">${item.title}</p>
-//     <p class="book-author">${item.author}</p>
-//     <button onclick="remove(${item.id})" type="button" id="book-${item.id}">Remove</button>
-
-//   `;
-//   bookContent.innerHTML = text;
-//   booksContainer.appendChild(bookContent);
-// };
-// // ===================================================================
-// const getData = () => {
-//   books = JSON.parse(localStorage.getItem('data')) || [];
-//   if (books?.length > 0) {
-//     booksContainer.innerHTML = '';
-//     books.forEach((book) => {
-//       showBook(book);
-//     });
-//   } else {
-//     booksContainer.innerHTML = 'No thing books here';
-//   }
-// };
-// // ==================================================================
-// form.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   const { title, author } = e.target;
-//   if (title.value === '' || author.value === '') {
-//     // eslint-disable-next-line no-alert
-//     alert('Please enter a title and author');
-//   } else {
-//     const data = {
-//       id: books?.length + 1,
-//       title: title.value,
-//       author: author.value,
-//     };
-//     books.push(data);
-//     localStorage.setItem('data', JSON.stringify(books));
-//     showBook(data);
-//     title.value = '';
-//     author.value = '';
-//   }
-// });
-// // ==============================================================
-// // eslint-disable-next-line no-unused-vars
-// function remove(c) {
-//   const updatedData = books.filter((book) => book.id !== c);
-//   localStorage.setItem('data', JSON.stringify(updatedData));
-//   getData();
-// }
-// getData();
-// // ===============================================================
-// start class Awesome
-
-// const form = document.querySelector('form');
+// eslint-disable-next-line max-classes-per-file
 const booksContainer = document.querySelector('.books-container');
 class Awesome {
   constructor(title, author) {
@@ -76,18 +14,25 @@ class Books {
 
   add(data) {
     if (data.title === '' || data.author === '') {
-          // eslint-disable-next-line no-alert
-          alert('Please enter a title and author');
-        } else {
-    const id = this.list.length + 1;
-    data.id = id;
-    console.log(this.list);
-    this.list.push(data);
-    localStorage.setItem('data', JSON.stringify(this.list));
-    this.show(data);
-        }
+      // eslint-disable-next-line no-alert
+      alert('Please enter a title and author');
+    } else {
+      const id = this.list.length + 1;
+      data.id = id;
+      this.list.push(data);
+      localStorage.setItem('data', JSON.stringify(this.list));
+      this.show(data);
+    }
   }
 
+  remove(c) {
+    const updatedData = this.list.filter((book) => book.id !== c);
+    this.list = updatedData;
+    localStorage.setItem('data', JSON.stringify(updatedData));
+    this.getData();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
   show(item) {
     if (booksContainer.innerHTML === 'No thing books here') {
       booksContainer.innerHTML = '';
@@ -96,7 +41,7 @@ class Books {
     const text = `
             <p class="book-title">${item.title}</p>
             <p class="book-author">${item.author}</p>
-            <button onclick="remove(${item.id})" type="button" id="book-${item.id}">Remove</button>
+            <button onclick="books.remove(${item.id})" type="button" id="book-${item.id}">Remove</button>
         
           `;
     bookContent.innerHTML = text;
@@ -121,9 +66,9 @@ this.addBtn.addEventListener('click', (e) => {
   const book = new Awesome();
   book.title = document.getElementById('title').value;
   book.author = document.getElementById('author').value;
+  // eslint-disable-next-line no-use-before-define
   books.add(book);
 });
 
 const books = new Books();
 books.getData();
-console.log(books);
