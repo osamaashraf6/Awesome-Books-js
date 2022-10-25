81c5a28fc648b008aa8e929adcfb31bb66801802
Martin Perez Bazan <martin.perezbazan@gmail.com>
2022-10-25 13:14:09 -0300

    Add method to show and add books

--- static/js/index.js
+++ static/js/index.js
@@ -59,13 +59,61 @@
 // getData();
 // // =================================================================================================
 // start class Awesome
+
+const form = document.querySelector('form');
+const booksContainer = document.querySelector('.books-container');
 class Awesome {
   constructor(title, author) {
     this.title = title;
     this.author = author;
-    this.addBtn = document.querySelector('#addButton');
-    this.addBtn.addEventListener('click', () => {
-      console.log('yes');
-    });
+    this.books = [];
   }
-}
+  show() {
+    if (booksContainer.innerHTML === 'No thing books here') {
+          booksContainer.innerHTML = '';
+        }
+        const bookContent = document.createElement('div');
+        const text = `
+          <p class="book-title">${this.title}</p>
+          <p class="book-author">${this.author}</p>
+          <button onclick="remove(${this.title})" type="button" id="book-${this.title}">Remove</button>
+      
+        `;
+        bookContent.innerHTML = text;
+        booksContainer.appendChild(bookContent);
+  }
+  add() {
+    console.log('ADDED');
+    const data = {
+      title: this.title, 
+      author: this.author
+    };
+    this.books.push(data);
+    
+    const dataFromStorage = JSON.parse(localStorage.getItem('data'));
+    let id = 1;
+    if(dataFromStorage){
+      id = dataFromStorage.length + 1;
+    }
+    data.id = id;
+    console.log(data)
+    let allData = [data]
+    if(dataFromStorage){
+     allData = [...dataFromStorage,data]
+    }
+    localStorage.setItem('data', JSON.stringify(allData));
+    title.value = '';
+    author.value = '';
+  }
+  }
+
+this.addBtn = document.querySelector('#addButton');
+this.addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const book1 = new Awesome();
+  book1.title = document.getElementById('title').value;
+  book1.author = document.getElementById('author').value;
+  book1.add();
+  book1.show();
+  console.log(book1);
+});
